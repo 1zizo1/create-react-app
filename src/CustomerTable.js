9885283815ed181d7cfbb1000f8d40af8a68562f
1zizo1 <ahmedmohmedzizo258@gmail.com>
2024-07-17 21:48:48 +0300

    add api

--- src/CustomerTable.js
+++ src/CustomerTable.js
@@ -8,7 +8,7 @@ const CustomerTable = () => {
   const [selectedCustomerId, setSelectedCustomerId] = useState(null);
 
   useEffect(() => {
-    fetch('https://server-beta-nine-98.vercel.app/data')
+    fetch('https://server-a2u372tmq-1zizo1s-projects.vercel.app/data')
       .then(response => response.json())
       .then(data => {
         setCustomers(data.customers);
